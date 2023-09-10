e5bc93cec9bf55e16c93bf122e496b3e3714c076
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-09-10 03:13:32 -0400

    implemented standard clock

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -4,6 +4,7 @@ import digitalio
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
+from time import strftime, sleep
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -64,8 +65,13 @@ while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=400)
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    y = 0
+    time = strftime("%m/%d/%Y %H:%M:%S")
+    draw.text((x, y), time, font=font, fill="#FFFFFF")
+
+    print (strftime("%m/%d/%Y %H:%M:%S"), end="", flush=True)
+    print("\r", end="", flush=True)
 
     # Display image.
     disp.image(image, rotation)
-    time.sleep(1)
+    sleep(1)
