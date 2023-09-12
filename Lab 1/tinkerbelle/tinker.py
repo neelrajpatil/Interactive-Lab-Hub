1e8fcc7fbbe82c0ca47167b18e79e4007b68fcc5
Neelraj Patil <neelrajpatil@ymail.com>
2023-09-12 01:45:20 -0400

    Fixed git issues

---
+++ Lab 1/tinkerbelle/tinker.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+import socket
+
+port=5001
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+def broadcast(name, value):
+	emit(name, value, broadcast=True, include_self=False)
+
+def get_ip_address():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# use a public address to bypass the need to connect
+	s.connect(("8.8.8.8", 80))
+	ip_address = s.getsockname()[0]
+	s.close()
+	return ip_address
+
+@socketio.on('connect')
+def test_connect():
+	print('connected')
+	emit('after connect',  {'data':'Lets dance'})
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+
+@socketio.on('hex')
+def handle_hex(val):
+	broadcast('hex', val)
+
+@socketio.on('blues')
+def handle_blues():
+    broadcast('blues', 'static/blues.gif')
+    
+@socketio.on('jazz')
+def handle_blues():
+    broadcast('blues', 'static/jazz.gif')
+    
+@socketio.on('rock')
+def handle_blues():
+    broadcast('blues', 'static/rocknroll.gif')
+
+
+@socketio.on('audio')
+def handle_audio(val):
+	broadcast('audio', val)
+
+@socketio.on('pauseAudio')
+def handle_pause(val):
+	broadcast('pauseAudio', val)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+
+	ip = get_ip_address()
+	if ip:
+		print(f"access at http://{ip}:{port}")
+	else:
+		print(f"No non-local IP found. Access may be available at http://127.0.0.1:{port}")
+	socketio.run(app, host='0.0.0.0', debug=True, port=port)
