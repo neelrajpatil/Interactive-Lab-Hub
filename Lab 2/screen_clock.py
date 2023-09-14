72c7cc2037492a266d13677cb11fca774237e366
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-09-14 14:42:27 -0400

    implemented main menu and difficulty

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -66,84 +66,127 @@ buttonB = digitalio.DigitalInOut(board.D24)
 buttonA.switch_to_input()
 buttonB.switch_to_input()
 
-wall_pos = [240, 120, 0]
-speed = 5
-jump_height = 7
-jump_count = 16
-jumps = 0
+while True: # main game loop
 
-line_width = 3
+    wall_pos = [240, 120, 0]
+    speed = 5
+    jump_height = 7
+    jump_count = 16
+    jumps = 0
 
-game_over = False
-lost_msg_1 = "GAME OVER"
-lost_msg_2 = "PRESS DOWN TO RESET"
+    line_width = 3
 
-ball_pos = [[40, 100], [60, 120]]
+    game_over = False
 
-bgrnd_clr = "#000000"
-objct_clr = "#FFFFFF"
+    ball_pos = [[40, 100], [60, 120]]
 
-start = time.time()
-current = start
+    bgrnd_clr = "#000000"
+    objct_clr = "#FFFFFF"
 
-i = 0
-i_max = 0
+    i = 0
+    i_max = 0
 
-while True:
-    current = time.time()
-    
+    chosen = False
+    difficulty = 0
+
     draw.rectangle((0, 0, width, height), outline=0, fill=bgrnd_clr)
-    draw.line([(0, 120), (240, 120)], fill=objct_clr, width=3)
+    disp.image(image, rotation)
+    sleep(0.5)
 
-    draw.text((20, 10), "TIME", font=font, fill=objct_clr)
-    draw.text((20, 30), str(i), font=font, fill=objct_clr)
-    draw.text((160, 10), "HIGH", font=font, fill=objct_clr)
-    draw.text((160, 30), str(i_max), font=font, fill=objct_clr)
+    while not chosen:
+        draw.rectangle((0, 0, width, height), outline=0, fill=bgrnd_clr)
 
-    if jumps == 0 and not buttonA.value:
-        jumps = jump_count
+        if not buttonB.value:
+            difficulty = (difficulty + 1) % 3
 
-    if jumps > jump_count // 2:
-        ball_pos[0][1] -= jump_height
-        ball_pos[1][1] -= jump_height
-        jumps -= 1
-    elif jumps > 0:
-        ball_pos[0][1] += jump_height
-        ball_pos[1][1] += jump_height
-        jumps -= 1
+        if not buttonA.value:
+            chosen = True
 
-    draw.ellipse([tuple(x) for x in ball_pos], fill=objct_clr, width=line_width)
+        draw.text((20, 50 + difficulty * 20), "-", font=font, fill=objct_clr)
 
-    wall_poses = [[(240 - wall_pos[k], 80), (240 - wall_pos[k], 120)] for k in range(3)]
+        draw.text((60, 10), "TIME KILLER", font=font, fill=objct_clr)
+        draw.text((40, 50), "EASY", font=font, fill=objct_clr)
+        draw.text((40, 70), "MEDIUM", font=font, fill=objct_clr)
+        draw.text((40, 90), "HARD", font=font, fill=objct_clr)
 
-    for k in range(3):
-        
-        wall_pos[k] = (wall_pos[k] + speed) % 240
-        draw.line(wall_poses[k], fill=objct_clr, width=line_width)
-        if (ball_pos[0][0] < 240 - wall_pos[k] < ball_pos[1][0] or \
-             ball_pos[0][0] < 240 - wall_pos[k] + line_width < ball_pos[1][0]) and \
-            ball_pos[1][1] >= 80:
-            draw.rectangle((0, 0, width, height), outline=0, fill=bgrnd_clr)
-            draw.text((60, 40), lost_msg_1, font=font, fill=objct_clr)
-            draw.text((10, 60), lost_msg_2, font=font, fill=objct_clr)
-            draw.text((60, 80), "SCORE: " + str(i), font=font, fill=objct_clr)
-            disp.image(image, rotation)
-            game_over = True
-
-    if game_over:
-        while game_over:
-            if not buttonB.value:
-                i_max = max(i_max, i)
-                i = 0
-                start = time.time()
-                current = start
-                jumps = 0
-                wall_pos = [240, 120, 0]
-                ball_pos = [[40, 100], [60, 120]]
-                game_over = False
-
-    else:
-        i = int(current - start)
-
-        # Display image.
         disp.image(image, rotation)
+        sleep(0.1)
+
+    draw.rectangle((0, 0, width, height), outline=0, fill=bgrnd_clr)
+    draw.text((70, 40), "GO!", font=ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 48), fill=objct_clr)
+    disp.image(image, rotation)
+    sleep(1)
+
+    start = time.time()
+    current = start
+    game = True
+
+    while game:
+        current = time.time()
+        
+        draw.rectangle((0, 0, width, height), outline=0, fill=bgrnd_clr)
+        draw.line([(0, 120), (240, 120)], fill=objct_clr, width=3)
+
+        draw.text((20, 10), "TIME", font=font, fill=objct_clr)
+        draw.text((20, 30), str(i), font=font, fill=objct_clr)
+        draw.text((160, 10), "HIGH", font=font, fill=objct_clr)
+        draw.text((160, 30), str(i_max), font=font, fill=objct_clr)
+
+        if jumps == 0 and not buttonA.value:
+            jumps = jump_count
+
+        if jumps > jump_count // 2:
+            ball_pos[0][1] -= jump_height
+            ball_pos[1][1] -= jump_height
+            jumps -= 1
+        elif jumps > 0:
+            ball_pos[0][1] += jump_height
+            ball_pos[1][1] += jump_height
+            jumps -= 1
+
+        draw.ellipse([tuple(x) for x in ball_pos], fill=objct_clr, width=line_width)
+
+        wall_poses = [[(240 - wall_pos[k], 80), (240 - wall_pos[k], 120)] for k in range(3)]
+
+        for k in range(3):
+            
+            wall_pos[k] = (wall_pos[k] + speed) % 240
+            draw.line(wall_poses[k], fill=objct_clr, width=line_width)
+            if (ball_pos[0][0] < 240 - wall_pos[k] < ball_pos[1][0] or \
+                ball_pos[0][0] < 240 - wall_pos[k] + line_width < ball_pos[1][0]) and \
+                ball_pos[1][1] >= 80:
+                draw.rectangle((0, 0, width, height), outline=0, fill=bgrnd_clr)
+                draw.text((60, 20), "GAME OVER", font=font, fill=objct_clr)
+                draw.text((60, 40), "SCORE: " + str(i), font=font, fill=objct_clr)
+                draw.text((60, 80), "A TO REPLAY", font=font, fill=objct_clr)
+                draw.text((0, 100), "B TO CHANGE DIFFICULTY", font=font, fill=objct_clr)
+                
+                disp.image(image, rotation)
+                game_over = True
+
+        if game_over:
+            while game_over:
+                if not buttonB.value:
+                    game = False
+                    break
+                if not buttonA.value:
+                    i_max = max(i_max, i)
+                    i = 0
+                    start = time.time()
+                    current = start
+                    jumps = 0
+                    wall_pos = [240, 120, 0]
+                    ball_pos = [[40, 100], [60, 120]]
+                    game_over = False
+                    draw.rectangle((0, 0, width, height), outline=0, fill=bgrnd_clr)
+                    draw.text((70, 40), "GO!", font=ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 48), fill=objct_clr)
+                    disp.image(image, rotation)
+                    sleep(0.5)
+
+        else:
+            i = int(current - start)
+
+            # Display image.
+            disp.image(image, rotation)
+
+        sleep(0.01 + (2 - difficulty) * 0.05)
