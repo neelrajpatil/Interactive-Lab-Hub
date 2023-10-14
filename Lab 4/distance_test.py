65fa1b62a321a378a169892063015394de30fbc0
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-10-14 15:02:45 -0400

    add distance test

--- Lab 4/distance_test.py
+++ Lab 4/distance_test.py
@@ -1,29 +1,10 @@
-"""
-	Reading distance from the laser based VL53L1X
-	This example prints the distance to an object. If you are getting weird
-	readings, be sure the vacuum tape has been removed from the sensor.
-"""
-
-import qwiic
 import time
+import board
+import busio
+import adafruit_vcnl4040
 
-print("VL53L1X Qwiic Test\n")
-ToF = qwiic.QwiicVL53L1X()
-if (ToF.sensor_init() == None):					 # Begin returns 0 on a good init
-	print("Sensor online!\n")
-
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_vcnl4040.VCNL4040(i2c)
 while True:
-	try:
-		ToF.start_ranging()						 # Write configuration bytes to initiate measurement
-		time.sleep(.005)
-		distance = ToF.get_distance()	 # Get the result of the measurement from the sensor
-		time.sleep(.005)
-		ToF.stop_ranging()
-
-		distanceInches = distance / 25.4
-		distanceFeet = distanceInches / 12.0
-
-		print("Distance(mm): %s Distance(ft): %s" % (distance, distanceFeet))
-
-	except Exception as e:
-		print(e)
+	print(sensor.proximity)
+	time.sleep(0.1)
